a5b5c5372333575756887e91cde62464099ff0c5
Ghostily-Boo <grace.rl.qin@gmail.com>
2020-11-20 20:44:16 -0500

    commit tree a6018ae30dff43dcc0f5a61285ea184014ef39fe

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,29 @@
-class Waiter
+require 'pry'
 
+class Waiter
+    attr_reader :name, :years
+
+    def initialize(name, years)
+        @name = name
+        @years = years
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip=0)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select {|meal| meal.waiter == self}
+    end
+
+    def best_tipper
+        tips = meals.map {|meal| meal.tip}
+        max_tip = tips.max
+        meals[tips.index(tips.max)].customer
+    end
 end
